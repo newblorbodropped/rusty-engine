fb6324fa185d5f0bc1b0f8fffb4e78ea89ca2ef9
newblorbojustdropped <ronnie.eshel.on.twitch@gmail.com>
2023-07-27 21:30:13 +0200

    added additional and more robust argument handling

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate glium;
 extern crate image;
 
 use std::env;
+use std::str::FromStr;
 
 use glium::glutin;
 use glium::glutin::event as ev;
@@ -15,23 +16,49 @@ mod drawing;
 mod event_handling;
 
 struct Params {
-    full_screen: bool
+    full_screen: bool,
+    post_pr_id: u16
 }
 
-fn parse_params(params: Vec<String>) -> Params {
-    if params.is_empty() {
-        Params{full_screen: false}
-    } else {
-        let mut params = params.iter();
-    params.next();
-        match params.next() {
-            Some(_) => Params{full_screen: true},
-            _ => Params{full_screen: false}
+fn parse_params(params: Vec<String>) -> Result<Params, String> {
+    let mut full_screen = false;
+    let mut post_pr_id : u16 = 0;
+
+    let mut param_iter = params.iter();
+
+    //throwing away the program name itself
+    let _ = param_iter.next(); 
+    
+    while let Some(param) = param_iter.next() {
+        match param.as_str() {
+            "-f" => {
+                full_screen = true;
+            },
+            "-p" => {
+                match param_iter.next() {
+                    None => { return Err("No shader for postprocessing given.".to_owned()); },
+                    Some(opt) => {
+                        match u16::from_str(opt.as_str()) {
+                            Ok(id) => { post_pr_id = id; },
+                            Err(_) => { return Err("Invalid shader for postprocessing given.".to_owned()); }
+                        }
+                    }
+                }
+            }
+            ukwn => {
+                let error_message = "Invalid argument given: ".to_owned() + ukwn;
+                return Err(error_message);
+            }
         }
     }
+
+    Ok(Params {
+        full_screen: full_screen,
+        post_pr_id: post_pr_id
+    })
 }
 
-fn event_handler_gen<T>(display: glium::Display) ->
+fn event_handler_gen<T>(display: glium::Display, post_pr_id: u16) ->
 impl FnMut(ev::Event<'_, T>, &evl::EventLoopWindowTarget<T>, &mut evl::ControlFlow){
     //load the models
     let mut cube = Mesh::new_with_id_shader_tex(1, 1, 1);
@@ -46,7 +73,7 @@ impl FnMut(ev::Event<'_, T>, &evl::EventLoopWindowTarget<T>, &mut evl::ControlFl
 
     //load the shader programs
     let shader_prog = ShaderProg::load_from_file(1, &display);
-    let shaderpp_prog = ShaderProg::load_from_file_pp(1, &display);
+    let shaderpp_prog = ShaderProg::load_from_file_pp(post_pr_id, &display);
 
     //create a camera
     let camera = event_handling::camera_transformations::Camera::default();
@@ -99,19 +126,32 @@ impl FnMut(ev::Event<'_, T>, &evl::EventLoopWindowTarget<T>, &mut evl::ControlFl
 
 fn main() {
     let params = parse_params(env::args().collect());
-    
-    let event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new();
-    let cb = glutin::ContextBuilder::new().with_depth_buffer(24);
-    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-    if params.full_screen {
-        let monitor_handle = display.gl_window().window().available_monitors().next().unwrap();
-        let fs = glutin::window::Fullscreen::Borderless(Some(monitor_handle));
-        display.gl_window().window().set_fullscreen(Some(fs));
-        display.gl_window().window().set_cursor_visible(false);
-        display.gl_window().window().set_cursor_grab(glutin::window::CursorGrabMode::Confined);
+    match params {
+        Ok(par) => {
+            let event_loop = glutin::event_loop::EventLoop::new();
+            let wb = glutin::window::WindowBuilder::new();
+            let cb = glutin::ContextBuilder::new().with_depth_buffer(24);
+            let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+            if par.full_screen {
+                let monitor_handle = display.gl_window().window().available_monitors().next().unwrap();
+                let fs = glutin::window::Fullscreen::Borderless(Some(monitor_handle));
+                display.gl_window().window().set_fullscreen(Some(fs));
+                display.gl_window().window().set_cursor_visible(false);
+                display.gl_window().window()
+                    .set_cursor_grab(glutin::window::CursorGrabMode::Confined)
+                    .or_else(|_e| {
+                        display.gl_window().window()
+                            .set_cursor_grab(glutin::window::CursorGrabMode::Locked)
+                    })
+                    .unwrap();
+            }
+            
+            event_loop.run(event_handler_gen(display, par.post_pr_id));
+        },
+        Err(message) =>  {
+            println!("{}", message);
+        }
     }
-    
-    event_loop.run(event_handler_gen(display));
 }
